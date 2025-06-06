115c2816b186de5125966159db81e80d7d1647a3
FINDERFEED <windingaster335@gmail.com>
2025-06-06 14:45:57 +0300

    flicker fix compatibility

--- src/main/java/com/finderfeed/fdbosses/client/BossClientMixinHandle.java
+++ src/main/java/com/finderfeed/fdbosses/client/BossClientMixinHandle.java
@@ -4,6 +4,8 @@ import com.finderfeed.fdbosses.BossClientEvents;
 import com.finderfeed.fdbosses.init.BossEffects;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.Options;
+import net.minecraft.util.Mth;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
@@ -16,7 +18,12 @@ public class BossClientMixinHandle {
     public static void darknessCalculate(LivingEntity entity, float pticks, CallbackInfoReturnable<Float> cir){
 
         if (entity.hasEffect(BossEffects.CHESED_DARKEN) || entity.hasEffect(BossEffects.CHESED_GAZE)) {
-            float value = cir.getReturnValue();
+//            float value = cir.getReturnValue();
+
+            //Hope it won't change in the future...
+            MobEffectInstance mobeffectinstance = entity.getEffect(MobEffects.NIGHT_VISION);
+            float value = !mobeffectinstance.endsWithin(200) ? 1.0F : 0.7F + Mth.sin(((float)mobeffectinstance.getDuration() - pticks) * (float) Math.PI * 0.2F) * 0.3F;
+
             float percent = 1 - Math.max(BossClientEvents.getChesedGazePercent(pticks), BossClientEvents.getChesedDarkenPercent(pticks));
             cir.setReturnValue(value * percent);
         }
--- src/main/java/com/finderfeed/fdbosses/mixin/GameRendererMixin.java
+++ src/main/java/com/finderfeed/fdbosses/mixin/GameRendererMixin.java
@@ -19,10 +19,10 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(GameRenderer.class)
+@Mixin(value = GameRenderer.class, priority = 0)
 public class GameRendererMixin {
 
-    @Inject(method = "getNightVisionScale",at = @At("RETURN"), cancellable = true)
+    @Inject(method = "getNightVisionScale",at = @At("HEAD"), cancellable = true)
     private static void nightVisionScale(LivingEntity entity, float pticks, CallbackInfoReturnable<Float> cir){
         BossClientMixinHandle.darknessCalculate(entity,pticks,cir);
     }
