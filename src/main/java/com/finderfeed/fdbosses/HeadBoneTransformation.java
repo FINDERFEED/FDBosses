6b35439f8ceeafeb7757cd06a31d65cdc9f87a40
FINDERFEED <windingaster335@gmail.com>
2025-06-17 21:50:22 +0300

    this shit is so ass

--- src/main/java/com/finderfeed/fdbosses/HeadBoneTransformation.java
+++ src/main/java/com/finderfeed/fdbosses/HeadBoneTransformation.java
@@ -18,101 +18,15 @@ public class HeadBoneTransformation<T extends Mob & AnimatedObject & IHasHead<T>
     @Override
     public void transformBone(T t, FDModel model, FDModelPart fdModelPart, PoseStack poseStack, MultiBufferSource multiBufferSource,String boneName, int light, int overlay, float v) {
 
-//        float yBodyRot = FDMathUtil.lerp(t.yRotO,t.getYRot(), v);
-//
-//
-//        HeadController<T> headController = t.getHeadController();
-//
-//        if (!headController.hasReachedDestination()){
-//            var rot = headController.getCurrentRotation(v);
-//            fdModelPart.setXRot(rot.x);
-//            fdModelPart.setYRot(rot.y);
-//        }else {
-//            if (headController.isTransitioningToLookTarget()) {
-//                float yHeadRot = t.getViewYRot(v);
-//                float xHeadRot = t.getViewXRot(v);
-//                fdModelPart.setYRot(yBodyRot - yHeadRot + fdModelPart.initRotation.y);
-//                fdModelPart.setXRot(-xHeadRot + fdModelPart.initRotation.x);
-//            }
-//        }
+        var vec = t.getHeadController().getCurrentHeadAngles(v);
 
-        Player player = t.level().getNearestPlayer(t, 30);
-
-        if (player == null) return;
+        fdModelPart.setXRot(vec.x);
+        fdModelPart.setYRot(vec.y);
 
 
-
-        Matrix4f transform = t.getModelPartTransformation(t, boneName, model, v);
-
-        Vector3f position = transform.transformPosition(new Vector3f());
-        Vector3f wposition = position.add(
-                (float) t.getX(),
-                (float) t.getY(),
-                (float) t.getZ(),
-                new Vector3f()
-        );
-
-        Vector3d playerPosition = new Vector3d(
-                player.getPosition(v).x,
-                player.getPosition(v).y + player.getEyeHeight(),
-                player.getPosition(v).z
-        );
-
-        Vector3d between = playerPosition.sub(wposition,new Vector3d());
-
-        Vector3d axisX = this.floatToDouble(transform.transformDirection(new Vector3f(1,0,0))).normalize();
-        Vector3d axisY = this.floatToDouble(transform.transformDirection(new Vector3f(0,1,0))).normalize();
-        Vector3d axisZ = this.floatToDouble(transform.transformDirection(new Vector3f(0,0,1))).normalize();
-
-
-        Vector3d vec = this.fromOtherBasisToEuclidian(axisX,axisY,axisZ,between);
-
-        float verticalAngle = 180 + (float)Math.toDegrees(Math.atan2(vec.x,vec.z));
-
-        double a = Math.sqrt(vec.z * vec.z + vec.x * vec.x);
-        float horizontalAngle =  (float) Math.toDegrees(Math.atan2(vec.y,a));
-
-
-//        fdModelPart.setXRot(-30);
-//        fdModelPart.setYRot(-10);
-
-        fdModelPart.setYRot(verticalAngle);
-        fdModelPart.setXRot(horizontalAngle);
 
     }
 
-    private Vector3d floatToDouble(Vector3f v){
-        return new Vector3d(
-                v.x,v.y,v.z
-        );
-    }
 
-    private Vector3d fromEuclidianToOtherBasis(
-            Vector3d nbx,
-            Vector3d nby,
-            Vector3d nbz,
-            Vector3d point
-    ){
-        Matrix3d matrix3d = new Matrix3d(
-                nbx.x,nbx.y,nbx.z,
-                nby.x,nby.y,nby.z,
-                nbz.x,nbz.y,nbz.z
-        );
-        return matrix3d.transform(point, new Vector3d());
-    }
-
-    private Vector3d fromOtherBasisToEuclidian(
-            Vector3d nbx,
-            Vector3d nby,
-            Vector3d nbz,
-            Vector3d point
-    ){
-        Matrix3d matrix3d = new Matrix3d(
-                nbx.x,nbx.y,nbx.z,
-                nby.x,nby.y,nby.z,
-                nbz.x,nbz.y,nbz.z
-        ).invert();
-        return matrix3d.transform(point, new Vector3d());
-    }
 
 }
--- src/main/java/com/finderfeed/fdbosses/HeadController.java
+++ src/main/java/com/finderfeed/fdbosses/HeadController.java
@@ -6,7 +6,10 @@ import com.finderfeed.fdlib.systems.bedrock.models.FDModelPart;
 import com.finderfeed.fdlib.util.math.FDMathUtil;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Mob;
-import org.joml.Vector2f;
+import net.minecraft.world.entity.player.Player;
+import org.joml.*;
+
+import java.lang.Math;
 
 public class HeadController<T extends Mob & IHasHead<T> & AnimatedObject> {
 
@@ -16,14 +19,10 @@ public class HeadController<T extends Mob & IHasHead<T> & AnimatedObject> {
 
     private float headTransitionSpeed;
 
-    private float currentRotationX;
-    private float currentRotationY;
-    private float oldRotationY;
-    private float oldRotationX;
-
-    private boolean rotatesToHeadTarget;
-
-    private boolean reachedDestination = true;
+    private float currentHeadRotationX = 0;
+    private float currentHeadRotationY = 0;
+    private float oldHeadRotationX = 0;
+    private float oldHeadRotationY = 0;
 
     public HeadController(FDModel model, String headBone, T entity){
         this.entity = entity;
@@ -33,96 +32,73 @@ public class HeadController<T extends Mob & IHasHead<T> & AnimatedObject> {
     }
 
     public void clientTick(){
-        this.rotate();
+
+
+        this.oldHeadRotationX = this.currentHeadRotationX;
+        this.oldHeadRotationY = this.currentHeadRotationY;
+
+        Vector2f angles = this.getLookAtTargetAngles();
+
+        float maxXRot = entity.getMaxHeadXRot();
+        float maxYRot = entity.getMaxHeadYRot();
+
+        float newY = this.getRotation(currentHeadRotationY, Mth.clamp(FDMathUtil.convertMCYRotationToNormal(angles.y), -maxYRot,maxYRot));
+        float newX = this.getRotation(currentHeadRotationX, Mth.clamp(FDMathUtil.convertMCYRotationToNormal(angles.x), -maxXRot,maxXRot));
+
+        this.currentHeadRotationY = newY;
+        this.currentHeadRotationX = newX;
+
     }
 
-    public void rotateToLookTarget(boolean rotate){
-        if (entity.level().isClientSide) {
-            entity.getAnimationSystem().applyAnimations(fdModel, 0f);
-            FDModelPart modelPart = fdModel.getModelPart(headBone);
-            if (rotate && !rotatesToHeadTarget) {
-                rotatesToHeadTarget = true;
-                reachedDestination = false;
-                this.setCurrentRotation(modelPart.getXRot(),modelPart.getYRot());
-            }else if (!rotate && rotatesToHeadTarget){
-                rotatesToHeadTarget = false;
-                reachedDestination = false;
-                this.setCurrentRotation(-entity.getXRot() + modelPart.initRotation.x, entity.getYRot() - entity.getYHeadRot() + modelPart.initRotation.y);
-            }
-            oldRotationX = this.getCurrentRotationX();
-            oldRotationY = this.getCurrentRotationY();
-        }
-    }
 
-    public boolean hasReachedDestination(){
-        return reachedDestination;
-    }
+    private Vector2f getLookAtTargetAngles(){
+        Player player = entity.level().getNearestPlayer(entity, 30);
 
-    protected void setCurrentRotation(float x, float y){
-        this.currentRotationX = FDMathUtil.convertMCYRotationToNormal(x);
-        this.currentRotationY = FDMathUtil.convertMCYRotationToNormal(y);
-    }
+        if (player == null) return new Vector2f();
 
-    public Vector2f getCurrentRotation(float pticks){
-        float rotx = FDMathUtil.lerpAround(oldRotationX,currentRotationX,-180,180,pticks);
-        float roty = FDMathUtil.lerpAround(oldRotationY,currentRotationY,-180,180,pticks);
 
-        return new Vector2f(
-               rotx, roty
+
+        Matrix4f transform = entity.getModelPartTransformation(entity, headBone, fdModel, 0);
+
+        Vector3f position = transform.transformPosition(new Vector3f());
+        Vector3f wposition = position.add(
+                (float) entity.getX(),
+                (float) entity.getY(),
+                (float) entity.getZ(),
+                new Vector3f()
         );
+
+        Vector3d playerPosition = this.floatToDouble(FDMathUtil.vec3ToVector3f(player.getEyePosition()));
+
+        Vector3d between = playerPosition.sub(wposition,new Vector3d());
+
+        Vector3d axisX = this.floatToDouble(transform.transformDirection(new Vector3f(1,0,0))).normalize();
+        Vector3d axisY = this.floatToDouble(transform.transformDirection(new Vector3f(0,1,0))).normalize();
+        Vector3d axisZ = this.floatToDouble(transform.transformDirection(new Vector3f(0,0,1))).normalize();
+
+
+        Vector3d vec = this.fromOtherBasisToEuclidian(axisX,axisY,axisZ,between);
+
+        float verticalAngle = 180 + (float)Math.toDegrees(Math.atan2(vec.x,vec.z));
+        double a = Math.sqrt(vec.z * vec.z + vec.x * vec.x);
+        float horizontalAngle =  (float) Math.toDegrees(Math.atan2(vec.y,a));
+
+        return new Vector2f(horizontalAngle,verticalAngle);
     }
 
-    //XY
-    public Vector2f getTargetRotation(){
-        entity.getAnimationSystem().applyAnimations(fdModel, 0f);
-        FDModelPart modelPart = fdModel.getModelPart(headBone);
-        if (rotatesToHeadTarget){
-            return new Vector2f(
-                    -entity.getXRot() + modelPart.initRotation.x,
-                    FDMathUtil.convertMCYRotationToNormal(entity.getYRot() - entity.getYHeadRot() + modelPart.initRotation.y)
-            );
-        }else{
-            return new Vector2f(
-                    modelPart.getXRot(),
-                    FDMathUtil.convertMCYRotationToNormal(modelPart.getYRot())
-            );
-        }
+
+
+    public float getHeadTransitionSpeed() {
+        return headTransitionSpeed;
     }
 
-    private void rotate(){
-        if (this.hasReachedDestination()) return;
-
-        Vector2f target = this.getTargetRotation();
-
-        oldRotationX = currentRotationX;
-        oldRotationY = currentRotationY;
-
-        float targetX = target.x;
-        float targetY = target.y;
-
-        this.rotateY(targetY);
-        this.rotateX(targetX);
-
-        if (Math.abs(targetY - currentRotationY) < 0.05 && Math.abs(targetX - currentRotationX) < 0.05){
-            this.reachedDestination = true;
-        }
-    }
-
-    private void rotateX(float targetX){
-        float newRotation = this.getRotation(currentRotationX, targetX);
-        currentRotationX = newRotation;
-    }
-
-    //-180 -> 180
-    private void rotateY(float targetY){
-        float newRotation = this.getRotation(currentRotationY, targetY);
-        currentRotationY = newRotation;
+    public T getEntity() {
+        return entity;
     }
 
     private float getRotation(float currentRotation, float targetY){
         if (currentRotation >= 0 && targetY >= 0 || currentRotation < 0 && targetY < 0){
-            int direction = targetY - currentRotationY > 0 ? 1 : -1;
-
+            int direction = targetY - currentRotation > 0 ? 1 : -1;
             if (currentRotation >= 0) {
                 if (direction == 1){
                     return Mth.clamp(currentRotation + this.getHeadTransitionSpeed() * direction, -Integer.MAX_VALUE,targetY);
@@ -164,33 +140,54 @@ public class HeadController<T extends Mob & IHasHead<T> & AnimatedObject> {
         return 0;
     }
 
-    public boolean isTransitioningToLookTarget(){
-        return this.rotatesToHeadTarget;
+    public float getCurrentHeadRotationX() {
+        return currentHeadRotationX;
     }
 
-    public float getCurrentRotationX() {
-        return currentRotationX;
+    public float getCurrentHeadRotationY() {
+        return currentHeadRotationY;
     }
 
-    public float getCurrentRotationY() {
-        return currentRotationY;
+    public Vector2f getCurrentHeadAngles(float pticks){
+        return new Vector2f(
+                FDMathUtil.lerpAround(oldHeadRotationX,currentHeadRotationX,-180,180,pticks),
+                FDMathUtil.lerpAround(oldHeadRotationY,currentHeadRotationY,-180,180,pticks)
+        );
     }
 
-    public float getOldRotationX() {
-        return oldRotationX;
+    //TO FDMATHHELPER
+    private Vector3d floatToDouble(Vector3f v){
+        return new Vector3d(
+                v.x,v.y,v.z
+        );
     }
 
-    public float getOldRotationY() {
-        return oldRotationY;
+    private Vector3d fromEuclidianToOtherBasis(
+            Vector3d nbx,
+            Vector3d nby,
+            Vector3d nbz,
+            Vector3d point
+    ){
+        Matrix3d matrix3d = new Matrix3d(
+                nbx.x,nbx.y,nbx.z,
+                nby.x,nby.y,nby.z,
+                nbz.x,nbz.y,nbz.z
+        );
+        return matrix3d.transform(point, new Vector3d());
     }
 
-    public float getHeadTransitionSpeed() {
-        if (true) return 2;
-        return headTransitionSpeed;
-    }
-
-    public T getEntity() {
-        return entity;
+    private Vector3d fromOtherBasisToEuclidian(
+            Vector3d nbx,
+            Vector3d nby,
+            Vector3d nbz,
+            Vector3d point
+    ){
+        Matrix3d matrix3d = new Matrix3d(
+                nbx.x,nbx.y,nbx.z,
+                nby.x,nby.y,nby.z,
+                nbz.x,nbz.y,nbz.z
+        ).invert();
+        return matrix3d.transform(point, new Vector3d());
     }
 
 }
