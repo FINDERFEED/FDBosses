2c38f7508193930b09cb0728328324363810e2e8
FINDERFEED <windingaster335@gmail.com>
2026-01-07 17:25:04 +0300

    lowered geburah damage and version number

--- src/main/java/com/finderfeed/fdbosses/config/GeburahConfig.java
+++ src/main/java/com/finderfeed/fdbosses/config/GeburahConfig.java
@@ -7,13 +7,13 @@ import net.minecraft.resources.ResourceLocation;
 public class GeburahConfig {
 
     @ConfigValue
-    public float coreRayStrikeDamage = 40;
+    public float coreRayStrikeDamage = 20;
 
     @ConfigValue
     public float rotatingLaserAttackDamage = 25;
 
     @ConfigValue
-    public float predictiveLaserAttackDamage = 40;
+    public float predictiveLaserAttackDamage = 30;
 
     @ConfigValue
     public float justiceHammerDamage = 40;
@@ -22,7 +22,7 @@ public class GeburahConfig {
     public float judgementBirdDamage = 15;
 
     @ConfigValue
-    public float earthquakeDamage = 40;
+    public float earthquakeDamage = 30;
 
     @ConfigValue
     public float cannonProjectileDamage = 30;
